be0d360d6595183feda886dff086a69a32c8acef
Vansh <vanshmorwani1@gmail.com>
2025-10-29 23:31:22 +0530

    booking schema added

---
+++ backend/src/models/Booking.ts
@@ -0,0 +1,42 @@
+import { Schema, model, Document } from "mongoose";
+
+export interface PromoDocument extends Document {
+  code: string
+  discountValue: number
+}
+
+const PromoSchema = new Schema<PromoDocument>({
+  code: { type: String, required: true, unique: true },
+  discountValue: { type: Number, required: true }
+})
+
+export interface BookingDocument extends Document {
+  experienceId: Schema.Types.ObjectId;
+  slotIndex: number;
+  name: string;
+  email: string;
+  promoCode?: string;
+  quantity: number;
+  totalAmount: number;
+  createdAt: Date;
+}
+
+const BookingSchema = new Schema<BookingDocument>(
+  {
+    experienceId: {
+      type: Schema.Types.ObjectId,
+      ref: "Experience",
+      required: true,
+    },
+    slotIndex: { type: Number, required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    promoCode: { type: String },
+    quantity: { type: Number, required: true, default: 1 },
+    totalAmount: { type: Number, required: true },
+  },
+  { timestamps: true }
+);
+
+export const Booking = model<BookingDocument>("Booking", BookingSchema);
+export const Promo = model<PromoDocument>("Promo", PromoSchema)
